942f104a8fd156fae8bf7f37947dcbd1584ddd27
Kritish Karmacharya <karmacharyakritish24@gmail.com>
2026-01-11 13:10:09 +0545

    Remove folder

--- Lab1IntroductionToJava/Program1.java
+++
@@ -1,8 +0,0 @@
-package Lab1IntroductionToJava;
-
-//Write a program to display “Welcome to Java Programming”.
-public class Program1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java Programming.");
-    }
-}
--- Lab4InheritanceAndPackaging/Package2/Package22.java
+++
@@ -1,9 +0,0 @@
-package Lab4InheritanceAndPackaging.Package2;
-
-import Lab4InheritanceAndPackaging.Package1.Package11;
-public class Package22 {
-    public static void main(String[] args){
-        Package11 obj=new Package11();
-        obj.sum();
-    }
-}
